7298b547df6e83aeb5f97fb4aef68c47f74e0e16
Rose Nguyen <nhn251@gmail.com>
2024-08-21 13:21:15 -0400

    customers to users

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -83,10 +83,10 @@ function Custom(props){
     console.log("YES loggedin");
     return (
       <div>
-    <Route exact path="/" render={() => (<Redirect to="/customers"/>)}/>
+    <Route exact path="/" render={() => (<Redirect to="/users"/>)}/>
     <Route path="/login" component={Login} />
     <Route path="/home" component={HomePage} />
-    <Route path="/customers" component={CustomersModule} />
+    <Route path="/users" component={CustomersModule} />
     <Route path="/events" component={EventsModule} />
     <Route path="/registrations" component={RegistrationsModule} />
     </div>
